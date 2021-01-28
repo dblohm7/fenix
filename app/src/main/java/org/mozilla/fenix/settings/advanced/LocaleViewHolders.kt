c25bc029d460ab1fd801e1c87956b6f73b5b0c7f
rxu <rxu@mozilla.com>
2021-01-28 16:56:09 +0800

    Change language display name

--- app/src/main/java/org/mozilla/fenix/settings/advanced/LocaleViewHolders.kt
+++ app/src/main/java/org/mozilla/fenix/settings/advanced/LocaleViewHolders.kt
@@ -20,14 +20,32 @@ class LocaleViewHolder(
 ) : BaseLocaleViewHolder(view, selectedLocale) {
 
     override fun bind(locale: Locale) {
-        // Capitalisation is done using the rules of the appropriate locale (endonym and exonym).
-        locale_title_text.text = locale.getDisplayName(locale).capitalize(locale)
-        // Show the given locale using the device locale for the subtitle.
-        locale_subtitle_text.text = locale.displayName.capitalize(Locale.getDefault())
+        if (locale.language == "zh") {
+            bindChineseLocale(locale)
+        } else {
+            // Capitalisation is done using the rules of the appropriate locale (endonym and exonym).
+            locale_title_text.text = locale.getDisplayName(locale).capitalize(locale)
+            // Show the given locale using the device locale for the subtitle.
+            locale_subtitle_text.text = locale.displayName.capitalize(Locale.getDefault())
+        }
         locale_selected_icon.isVisible = isCurrentLocaleSelected(locale, isDefault = false)
 
         itemView.setOnClickListener {
             interactor.onLocaleSelected(locale)
+        }
+    }
+
+    private fun bindChineseLocale(locale: Locale) {
+        if (locale.country == "CN") {
+            locale_title_text.text =
+                Locale.forLanguageTag("zh-Hans").getDisplayName(locale).capitalize(locale)
+            locale_subtitle_text.text =
+                Locale.forLanguageTag("zh-Hans").displayName.capitalize(Locale.getDefault())
+        } else if (locale.country == "TW") {
+            locale_title_text.text =
+                Locale.forLanguageTag("zh-Hant").getDisplayName(locale).capitalize(locale)
+            locale_subtitle_text.text =
+                Locale.forLanguageTag("zh-Hant").displayName.capitalize(Locale.getDefault())
         }
     }
 }
@@ -41,7 +59,12 @@ class SystemLocaleViewHolder(
     override fun bind(locale: Locale) {
         locale_title_text.text = itemView.context.getString(R.string.default_locale_text)
         // Use the device locale for the system locale subtitle.
-        locale_subtitle_text.text = locale.getDisplayName(locale).capitalize(locale)
+        if (locale.language == "zh" && locale.country == "TW") {
+            locale_subtitle_text.text =
+                Locale.forLanguageTag("zh-Hant").displayName.capitalize(Locale.getDefault())
+        } else {
+            locale_subtitle_text.text = locale.getDisplayName(locale).capitalize(locale)
+        }
         locale_selected_icon.isVisible = isCurrentLocaleSelected(locale, isDefault = true)
         itemView.setOnClickListener {
             interactor.onDefaultLocaleSelected()
